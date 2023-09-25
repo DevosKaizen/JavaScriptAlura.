3cfd91c78a78d63011cd8f57ad5033b74b70046a
AndrewDevos <andrew.cottadevos@gmail.com>
2023-09-24 21:58:57 -0300

    inseri comentarios

--- type-boolean.js
+++ type-boolean.js
@@ -7,3 +7,13 @@ const texto1 = "Alura";
 const texto2 = "alura"
 
 console.log( texto1 === texto2);
+
+// const texto3 = 'null';
+
+// if ( texto3 === null) {
+//     console.log("não a valor adribuido a variavel, por favor digite o valor a ser guardado! ");
+//     Input.value(texto3);
+// } else {
+//     console.log("o texto armazenado foi ");
+//     console.log(texto3);
+// }
