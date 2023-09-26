413cf1e4a5d56c8d9775976798a308b38cc4a73d
AndrewDevos <andrew.cottadevos@gmail.com>
2023-09-26 15:22:17 -0300

    madulo 1 finalizado quase metade do curso completo

--- type-number.js
+++ type-number.js
@@ -2,7 +2,7 @@
 
 const meuNumero = 3;
 
-const primeiroNumero = 1;
+let primeiroNumero = 1;
 const segundoNumero = 2;
 
 const operacaoMatematica =  primeiroNumero + segundoNumero;
@@ -11,3 +11,9 @@ console.log(operacaoMatematica);
 
 const alura = "Alura";
 console.log(alura * primeiroNumero)
+
+console.log('\n\n\n_____________________')
+primeiroNumero = 50;
+console.log('\n a const é: ', primeiroNumero)
+console.log(' o tipo é:', typeof primeiroNumero) // verifica o tipo com typeof
+console.log('_____________________')
--- type-string.js
+++ type-string.js
@@ -16,3 +16,5 @@ console.log("\n\n ===" )
 console.log("\n\n" )
 console.log(nome === nome2);
 console.log(senha.length);
+
+// Variáveis declaradas com var ou let podem ser reatribuídas. Porém, uma variável declarada com let só pode ser usada no mesmo escopo em que está definida. No caso da declaração const, significa que a variável sempre terá uma referência ao mesmo objeto ou valor primitivo, porque essa referência não pode ser alterada. A referência em si é imutável, mas o valor mantido pela variável não se torna imutável.
