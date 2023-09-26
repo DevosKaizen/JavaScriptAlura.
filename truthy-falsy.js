9134d3bdb17093101fe1447f01c8b581b50a649b
AndrewDevos <andrew.cottadevos@gmail.com>
2023-09-25 22:12:04 -0300

    semibooleanos

---
+++ truthy-falsy.js
@@ -0,0 +1,15 @@
+// boolean
+
+const usuarioLogado = true
+const contapagada = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false);
+console.log("" == false);
+
+console.log(1 == false);
+console.log("a" == false);
